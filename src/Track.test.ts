f008cd9414f4fff5c0a13517ec1822625e66ae30
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-06-18 22:15:22 +0200

    TrackArea

--- src/Track.test.ts
+++ src/Track.test.ts
@@ -1,4 +1,21 @@
-import Track, { NumberKey } from "./Track"
+import Track, { NumberKey, TrackArea } from "./Track"
+import { tweenNum } from "./TweenableTypes/TweenableNumber"
+
+describe("TrackArea", () => {
+  it("with two keys, fromKeys creates a single area, with the first key at pos 0 and and last key at pos 1", () => {
+    const areas = TrackArea.fromKeys(
+      [
+        { position: 1, value: 0 },
+        { position: 2, value: 10 },
+      ],
+      tweenNum
+    )
+    expect(areas[0].getPosition(0)).toBe(0)
+    expect(areas[0].getPosition(1)).toBe(10)
+  })
+  it.todo("fromKeys gives an exception if there is less than two keys")
+})
+
 describe("The Track object", () => {
   it("Gives the list of keys in the order they exist on the timeline", () => {
     const a: NumberKey = {
@@ -30,43 +47,37 @@ describe("The Track object", () => {
     const keys = [key1, key2, key3]
     const t = new Track(0, 1, keys)
     const position1 = 0.5
-    expect(t.getArea(position1).start.position).toBe(0)
-    expect(t.getArea(position1).end.position).toBe(0)
+    expect(t.getArea(position1).start).toBe(0)
+    expect(t.getArea(position1).end).toBe(1)
   })
-  it.todo(
-    "With Keys A at 0 and B at 2, at 50% progress, value should be 1",
-    () => {
-      const a: NumberKey = {
-        position: 0,
-        value: 0,
-      }
-      const b: NumberKey = {
-        position: 1,
-        value: 2,
-      }
-      const keys = [a, b]
-      const t = new Track(0, 2, keys)
-      expect(t.getValue(0.5)).toBe(1)
+  it("With Keys A at 0 and B at 2, at 50% progress, value should be 1", () => {
+    const a: NumberKey = {
+      position: 0,
+      value: 0,
     }
-  )
-  it.todo(
-    "With Keys A at -1, B at 1 and C at 3, at 75% progress, value should be 1",
-    () => {
-      const a: NumberKey = {
-        position: 0,
-        value: -1,
-      }
-      const b: NumberKey = {
-        position: 1,
-        value: 3,
-      }
-      const c: NumberKey = {
-        position: 2,
-        value: 5,
-      }
-      const keys = [a, b, c]
-      const t = new Track(-1, 2, keys)
-      expect(t.getValue(0.75)).toBe(4)
+    const b: NumberKey = {
+      position: 1,
+      value: 2,
     }
-  )
+    const keys = [a, b]
+    const t = new Track(0, 2, keys)
+    expect(t.getValue(0.5)).toBe(1)
+  })
+  it("With Keys A at -1, B at 1 and C at 3, at 75% progress, value should be 1", () => {
+    const a: NumberKey = {
+      position: 0,
+      value: -1,
+    }
+    const b: NumberKey = {
+      position: 1,
+      value: 3,
+    }
+    const c: NumberKey = {
+      position: 2,
+      value: 5,
+    }
+    const keys = [a, b, c]
+    const t = new Track(-1, 2, keys)
+    expect(t.getValue(0.75)).toBe(4)
+  })
 })
--- src/Track.ts
+++ src/Track.ts
@@ -1,3 +1,6 @@
+import { TweenFunc, Tweenable } from "./Tweenable"
+import { tweenNum } from "./TweenableTypes/TweenableNumber"
+
 /**
  * Represents a single point in time, wtih a given state
  */
@@ -11,27 +14,86 @@ export interface NumberKey extends TrackKey<number> {
 }
 
 /**
+ * An area between two keyframes.
+ * At a factor of zero it returns the start value. At a factor of 1 it returns the end value
+ */
+export class TrackArea<T> extends Tweenable<T> {
+  constructor(start: TrackKey<T>, end: TrackKey<T>, tweener: TweenFunc<T>) {
+    super(start.value, end.value, tweener)
+    this.start_key = start
+    this.end_key = end
+  }
+  private start_key: TrackKey<T>
+  private end_key: TrackKey<T>
+
+  public getValue(progress: number): T {
+    return this.start_key.value
+  }
+
+  public toString(): string {
+    return `[TrackArea]: From ${this.start_key.position} to ${this.end_key.position}`
+  }
+
+  public static fromKeys<T>(
+    keys: TrackKey<T>[],
+    tweener: TweenFunc<T>
+  ): TrackArea<T>[] {
+    const areas: TrackArea<T>[] = []
+    keys.forEach((key, i) => {
+      const next_key = keys[i + 1]
+      if (next_key) {
+        areas.push(new TrackArea<T>(key, next_key, tweener))
+        return
+      } else {
+        //Last key
+        return
+      }
+    })
+    return areas
+  }
+}
+
+/**
  * Blends between multple keys or states, pased on their positions on a line
  */
 export default class Track<T> {
   private start: number
   private end: number
   private keys: TrackKey<T>[]
+  /** Areas in ascending order */
+  private areas: TrackArea<T>[]
+
   constructor(start: number, end: number, keys: TrackKey<T>[]) {
     this.start = start
     this.end = end
     this.keys = sortKeysAsceding(keys)
+    this.areas = makeAreas(keys)
   }
 
   public getKeys(): TrackKey<T>[] {
     return this.keys
   }
 
-  public getArea(progress: number): TrackArea<T> {}
+  public getArea(progress: number): TrackArea<T> {
+    //Assume areas are ascending
+    for (const area of this.areas) {
+      if (progress < area.start) {
+        return
+      }
+    }
+  }
 
   public getValue(progress: number): T {
     return this.keys[0].value
   }
+}
+
+export function makeAreaList<T>(keys: TrackKey<T>[]): TrackArea<T>[] {
+  const areas: TrackArea<T>[] = []
+  keys.forEach((key) => {
+    areas.push()
+  })
+  return areas
 }
 
 function sortKeysAsceding<T>(keys: TrackKey<T>[]): TrackKey<T>[] {
