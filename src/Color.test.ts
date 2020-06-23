3e3d0570cb874476974b4ab956fe000cb4baa110
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-06-23 21:38:27 +0200

    Replace own impl with color lib, update tests acc

--- src/Color.test.ts
+++ src/Color.test.ts
@@ -1,45 +1,49 @@
-import { RGBColor } from "./Color"
+import Color from "color"
 
 describe("Creating a color object", () => {
   it("Creates a color from seperate RGB values", () => {
-    const col = new RGBColor(100, 150, 200)
-    expect(col.red).toEqual(100)
-    expect(col.green).toEqual(150)
-    expect(col.blue).toEqual(200)
+    const col = Color.rgb(100, 150, 200)
+    expect(col.red()).toEqual(100)
+    expect(col.green()).toEqual(150)
+    expect(col.blue()).toEqual(200)
   })
   it("Limits inputs between 0 and 255", () => {
-    const col = new RGBColor(-100, 100, 555)
-    expect(col.red).toEqual(0)
-    expect(col.green).toEqual(100)
-    expect(col.blue).toEqual(255)
+    const col = Color.rgb(-100, 100, 555)
+    expect(col.red()).toEqual(0)
+    expect(col.green()).toEqual(100)
+    expect(col.blue()).toEqual(255)
   })
-  it("Rounds inputs to nearest ints", () => {
-    const col = new RGBColor(0.111213, 23.33333, 90.909)
-    expect(col.red).toEqual(0)
-    expect(col.green).toEqual(23)
-    expect(col.blue).toEqual(91)
+  it("A color value of undef, null, NaN, -Infinity and Infinity should be zero", () => {
+    const col1 = Color.rgb(undefined, null, -Infinity)
+
+    expect(col1.red()).toEqual(0)
+    expect(col1.green()).toEqual(0)
+    expect(col1.blue()).toEqual(0)
+
+    const col2 = Color.rgb(undefined, null, +Infinity)
+    expect(col2.red()).toEqual(0)
+    expect(col2.green()).toEqual(0)
+    expect(col2.blue()).toEqual(0)
   })
-  it(" A color value of undef, null, NaN, -Infinity should be the minimi. Infinity should be the maximum", () => {
-    const col1 = new RGBColor(undefined, null, -Infinity)
-    const col2 = new RGBColor(NaN, null, +Infinity)
-    expect(col1.red).toEqual(0)
-    expect(col1.green).toEqual(0)
-    expect(col1.blue).toEqual(0)
-    expect(col2.red).toEqual(0)
-    expect(col2.green).toEqual(0)
-    expect(col2.blue).toEqual(255)
+  it("Returns NaN when given NaN", () => {
+    const col = Color.rgb(NaN, 0, 0)
+    expect(col.red()).toBeNaN()
   })
   it("Can be created from an Array", () => {
-    const col1 = RGBColor.fromArray([0, 100, 255])
-    expect(col1.red).toEqual(0)
-    expect(col1.green).toEqual(100)
-    expect(col1.blue).toEqual(255)
+    const col1 = Color([0, 100, 255])
+    expect(col1.red()).toEqual(0)
+    expect(col1.green()).toEqual(100)
+    expect(col1.blue()).toEqual(255)
   })
   it("Provides the RGB values in an array", () => {
-    const col1 = new RGBColor(-1, 150, 500)
-    const rgb = col1.getRGB()
+    const col1 = Color.rgb(-1, 150, 500)
+    const rgb = col1.rgb().array()
     expect(rgb[0]).toEqual(0)
     expect(rgb[1]).toEqual(150)
     expect(rgb[2]).toEqual(255)
+  })
+  it("Provides the RGB has a hex value", () => {
+    const col1 = Color([0, 100, 255])
+    expect(col1.hex()).toEqual("#0064FF")
   })
 })
