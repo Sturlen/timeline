0a7180a780915b738d87e0517edb354abe452f5d
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2020-07-06 11:38:40 +0200

    Fixed some typings

--- src/Timeline.test.ts
+++ src/Timeline.test.ts
@@ -1,13 +1,26 @@
-import Timeline, {TimelineTrack} from "./Timeline"
-import {tweenNum} from "./TweenableTypes/TweenableNumber"
-import {lerpColor} from "./TweenableTypes/TweenableColor"
+import Timeline, { TimelineTrack } from "./Timeline"
+import { tweenNum } from "./TweenableTypes/TweenableNumber"
+import { lerpColor } from "./TweenableTypes/TweenableColor"
 import Color from "color"
 
+const basic_color: TimelineTrack<Color> = {
+  name: "basic_color",
+  tweener: lerpColor,
+  keys: [
+    { position: 0, value: Color("white") },
+    { position: 1, value: Color("black") },
+  ],
+}
+const basic_number: TimelineTrack<number> = {
+  name: "time",
+  tweener: tweenNum,
+  keys: [
+    { position: 0, value: 0 },
+    { position: 1, value: 1 },
+  ],
+}
 
 it("is created from the keys you want to tween", () => {
-    const t = new Timeline([
-        {name: "sun_color", tweener: lerpColor, keys: []},
-        {name: "time", tweener: tweenNum, keys: []}
-    ])
-    expect(t).toBeInstanceOf(Timeline)
+  const t = new Timeline([basic_color, basic_number])
+  expect(t).toBeInstanceOf(Timeline)
 })
--- src/Timeline.ts
+++ src/Timeline.ts
@@ -1,10 +1,11 @@
-import { TweenFunc } from "./Tweenable";
-import TrackKey from "./TrackKey";
+import { TweenFunc } from "./Tweenable"
+import TrackKey from "./TrackKey"
+import { Track } from "./index"
 
 export interface TimelineTrack<T> {
-    name: string
-    tweener: TweenFunc<T>,
-    keys: TrackKey<T>[]
+  name: string
+  tweener: TweenFunc<T>
+  keys: TrackKey<T>[]
 }
 
 /**
@@ -12,9 +13,10 @@ export interface TimelineTrack<T> {
  * TODO: Better typing
  */
 export default class Timeline {
-    public tracks: TimelineTrack<any>[]
-    constructor(tracks: TimelineTrack<any>[]) {
-        this.tracks = tracks
-    }
-
+  public tracks: Track<unknown>[]
+  constructor(tracks: TimelineTrack<unknown>[]) {
+    this.tracks = tracks.map(<T>(input: TimelineTrack<T>) => {
+      return new Track<T>(0, 0, input.keys, input.tweener)
+    })
+  }
 }
